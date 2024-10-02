09b26c6ac8994ef28e84dfca9ecc2e4dc8919ed3
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-10-02 20:18:30 +0900

    Update 6. 푸드 파이터 대회.py

--- 프로그래머스Lv1/6. 푸드 파이터 대회.py
+++ 프로그래머스Lv1/6. 푸드 파이터 대회.py
@@ -1,9 +1,7 @@
 def solution(food):
     answer = ''
-    
+
     for i in range(len(food)):
-        value = food[i]//2
-        if value > 0:
-            answer += str(i)*value
-    answer += '0' + answer[::-1]
-    return answer
+        if food[i] > 1:
+            answer += str(i)*(food[i]//2)
+    return answer + "0" + answer[::-1]
