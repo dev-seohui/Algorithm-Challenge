37a3c68eb322c2d9f3b793c3ec0b3cd541b425a3
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-10-04 20:36:14 +0900

    Update 8. 같은 숫자는 싫어.py

--- 프로그래머스Lv1/8. 같은 숫자는 싫어.py
+++ 프로그래머스Lv1/8. 같은 숫자는 싫어.py
@@ -1,8 +1,8 @@
 def solution(arr):
     answer = []
-    answer.append(arr[0])
-    
-    for a in arr[1:]:
-        if a is not answer[-1]:
-            answer.append(a)
+
+    for i in range(len(arr)):
+        if not answer or arr[i] != answer[-1]:
+            answer.append(arr[i])
+
     return answer
