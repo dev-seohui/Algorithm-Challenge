f6468985eaaa5654fc8d00318fb3603aa14bbbaf
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-10-07 20:34:46 +0900

    Update 12. K번째수.py

--- 프로그래머스Lv1/12. K번째수.py
+++ 프로그래머스Lv1/12. K번째수.py
@@ -1,7 +1,8 @@
 def solution(array, commands):
     answer = []
-    for c in commands:
-        part = array[c[0]-1:c[1]]
-        part.sort()
-        answer.append(part[c[2]-1])
+
+    for command in commands:
+        i, j, k = command
+        sliced_array = sorted(array[i-1:j])
+        answer.append(sliced_array[k-1])
     return answer
