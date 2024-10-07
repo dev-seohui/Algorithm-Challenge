f4791d7c50beb42338dccc47d5ff78619e133568
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-10-07 20:20:22 +0900

    Update 11. 최소직사각형.py

--- 프로그래머스Lv1/11. 최소직사각형.py
+++ 프로그래머스Lv1/11. 최소직사각형.py
@@ -1,8 +1,11 @@
 def solution(sizes):
-    w = []
-    h = []
-    for s in sizes:
-        s.sort()
-        w.append(s[0])
-        h.append(s[1])
-    return max(w)*max(h)
+    bigger = max(sizes[0][0], sizes[0][1])
+    smaller = min(sizes[0][0], sizes[0][1])
+
+    for i in range(1, len(sizes)):
+        if max(sizes[i][0], sizes[i][1]) > bigger:
+            bigger = max(sizes[i][0], sizes[i][1])
+        if min(sizes[i][0], sizes[i][1]) > smaller:
+            smaller = min(sizes[i][0], sizes[i][1])
+
+    return bigger*smaller
