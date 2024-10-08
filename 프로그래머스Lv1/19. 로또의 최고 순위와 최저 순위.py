8f4892251db08ecfbaecfe4a5a7378e4d415153e
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-10-08 21:26:42 +0900

    Update 19. 로또의 최고 순위와 최저 순위.py

--- 프로그래머스Lv1/19. 로또의 최고 순위와 최저 순위.py
+++ 프로그래머스Lv1/19. 로또의 최고 순위와 최저 순위.py
@@ -1,5 +1,4 @@
 
-# 첫번째 풀이
 def solution(lottos, win_nums):
     
     min_rank, max_rank = 7, 7
@@ -11,27 +10,3 @@ def solution(lottos, win_nums):
             max_rank -= 1
     return min(max_rank, 6), min(min_rank, 6)
 
-
-
-# 두번쨰 풀이
-def solution(lottos, win_nums):
-    answer = []
-    result = [0, 0]
-
-    dup = len(set(lottos) & set(win_nums))  # 중복값 개수
-    zero = lottos.count(0)                  # 0의 개수 
-
-    count1 = dup                            # 최소
-    count2 = dup + zero                     # 최대
-
-    if count1 == 0:
-        if count2 == 0:
-            result[0] = result[1] = 6
-        else:
-            result[0] = 7 - count2
-            result[1] = 6
-    else:
-        result[0] = 7 - count2
-        result[1] = 7 - count1
-
-    return result
