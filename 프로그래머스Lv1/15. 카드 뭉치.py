0bdad9197610ec2199a567d9bf09e61717cb3d7e
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-10-08 18:44:53 +0900

    Update 15. 카드 뭉치.py

--- 프로그래머스Lv1/15. 카드 뭉치.py
+++ 프로그래머스Lv1/15. 카드 뭉치.py
@@ -1,17 +1,11 @@
 def solution(cards1, cards2, goal):
-    answer = []
-    
+
     for g in goal:
-        if len(cards1)>0 and g==cards1[0]:
-            answer.append(cards1[0])
-            cards1 = cards1[1:]
-        elif len(cards2)>0 and g==cards2[0]:
-            answer.append(cards2[0])
-            cards2 = cards2[1:]
+        if cards1 and g == cards1[0]:
+            cards1.pop(0)
+        elif cards2 and g == cards2[0]:
+            cards2.pop(0)
         else:
             return "No"
-        
-    if answer == goal:
-        return "Yes"
-    else:
-        return "No"
+    return "Yes"
+
