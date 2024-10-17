d7eaa0c47adb7df00e0af7ba19f7c6fb94653ea2
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-10-17 20:21:03 +0900

    Create 11. 카펫.py

---
+++ 프로그래머스Lv2/11. 카펫.py
@@ -0,0 +1,16 @@
+
+def solution(brown, yellow):
+    answer = [0, 0]
+    width, height = 0, 0
+    
+    divisor = []
+    for i in range(1, int(yellow**0.5)+1):
+        if yellow%i == 0:
+            width, height = max(i, yellow//i), min(i, yellow//i)
+            divisor.append([width, height])
+    
+    for d in divisor:
+        if brown == ((d[0]+d[1])*2+4):
+            answer[0] = d[0]+2
+            answer[1] = d[1]+2
+    return answer
