23dc380c0b24ed5ad3b1ac66576d7a690322be2c
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-10-22 19:31:28 +0900

    Create 12. 귤 고르기.py

---
+++ 프로그래머스Lv2/12. 귤 고르기.py
@@ -0,0 +1,19 @@
+
+def solution(k, tangerine):
+    answer = 0
+    dic = {}
+    
+    for t in tangerine:
+        if t not in dic:
+            dic[t] = 1
+        else:
+            dic[t] += 1
+    sorted_dic = sorted(dic.items(), key=lambda x:x[1], reverse=True)
+
+    
+    for size, count in sorted_dic:
+        k -= count
+        answer += 1
+        if k <= 0:
+            break
+    return answer
