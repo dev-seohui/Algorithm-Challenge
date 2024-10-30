2f467f667eabd440fe316cb852de8aabd4763c6a
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-10-30 19:13:13 +0900

    Create 42. 추억 점수.py

---
+++ 프로그래머스Lv1/42. 추억 점수.py
@@ -0,0 +1,11 @@
+
+def solution(name, yearning, photo):
+    answer = []
+    
+    for p in photo:
+        score = 0
+        for i in range(len(name)):
+            if name[i] in p:
+                score += yearning[i]
+        answer.append(score)
+    return answer
