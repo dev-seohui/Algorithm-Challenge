8b566930059bc12dcc11de4d07feab22460edcc6
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-11-01 20:46:38 +0900

    Create 15. 구명보트.py

---
+++ 프로그래머스Lv2/15. 구명보트.py
@@ -0,0 +1,18 @@
+
+from collections import deque
+
+def solution(people, limit):
+    answer = 0
+    
+    people.sort()
+    deque_people = deque(people)
+    
+    while deque_people:
+        left = deque_people.popleft()
+        if not deque_people:
+            return answer + 1
+        right = deque_people.pop()
+        if left + right > limit:
+            deque_people.appendleft(left)
+        answer += 1
+    return answer
