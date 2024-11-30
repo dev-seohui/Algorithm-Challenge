986234ee92d9fafea47183b44feb1472093556c6
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-12-01 00:30:43 +0900

    Create 최댓값과 최솟값.py

---
+++ Math/최댓값과 최솟값.py
@@ -0,0 +1,9 @@
+def solution(s):
+    
+    split_s = s.split()
+    new_s = [int(x) for x in split_s]
+    min_s = str(min(new_s))
+    max_s = str(max(new_s))
+    print(min_s, max_s)
+    answer = min_s + " " + max_s
+    return answer
