807c3654f64e90109e2ebef105ee73f6134d7fe4
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-12-01 00:13:08 +0900

    Create 삼총사.py

---
+++ Implementation/삼총사.py
@@ -0,0 +1,11 @@
+def solution(number):
+    
+    length = len(number)
+    count = 0
+    
+    for i in range(length-2):
+        for j in range(i+1, length-1):
+            for k in range(j+1, length):
+                if number[i]+number[j]+number[k] == 0:
+                    count += 1
+    return count
