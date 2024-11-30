4c0b40430e1f8386d9b3cd518ccadda0314380e0
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-12-01 00:46:41 +0900

    Create 의상.py

---
+++ Hash/의상.py
@@ -0,0 +1,11 @@
+def solution(clothes):
+    answer = 1
+    dic = {}
+    
+    for clothe in clothes:
+        dic[clothe[1]] = dic.get(clothe[1], 0) + 1
+    
+    for d in dic.values():
+        answer *= (d+1)
+    
+    return answer-1
