92a092dfe8de27115704845d0c0c98810efe1ad2
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-12-01 00:13:32 +0900

    Create 시저 암호.py

---
+++ Implementation/시저 암호.py
@@ -0,0 +1,15 @@
+def solution(s, n):
+    answer = ''
+    alphabet = "abcdefghijklmnopqrstuvwxyz" #26자
+    
+    for i in s:
+        if (i != " " and i == i.lower()): 
+            num = (alphabet.index(i.lower())+n)%26
+            answer += alphabet[num]
+        elif (i != " " and i == i.upper()): 
+            num = (alphabet.index(i.lower())+n)%26
+            answer += alphabet[num].upper()
+        else:
+            answer += " "
+    return answer
+    
