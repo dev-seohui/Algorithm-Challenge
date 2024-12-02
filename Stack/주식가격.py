efb0aee857861f8bc8305fef4089b68a6fefe775
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2024-12-02 13:13:07 +0900

    Create 주식가격.py

---
+++ Stack/주식가격.py
@@ -0,0 +1,15 @@
+def solution(prices):
+    n = len(prices)
+    answer = [0]*n
+    stack = []
+    
+    for i in range(n):
+        while stack and prices[stack[-1]] > prices[i]:
+            idx = stack.pop()
+            answer[idx] = i - idx
+        stack.append(i)
+    
+    while stack:
+        idx = stack.pop()
+        answer[idx] = n - idx - 1
+    return answer
