cc6b17c1596fa83e759709e557ade62c69f54a19
toughcookieseohui <142233835+toughcookieseohui@users.noreply.github.com>
2025-01-10 04:48:19 +0900

    Update 연결 요소의 개수.py

--- BFS DFS/연결 요소의 개수.py
+++ BFS DFS/연결 요소의 개수.py
@@ -4,16 +4,16 @@ input = sys.stdin.readline
 
 def dfs(node):
     visited[node] = True
-    for neighbor in graph[node]:
+    for neighbor in adjacent_list[node]:
         if not visited[neighbor]:
             dfs(neighbor)
 
 n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
+adjacent_list = [[] for _ in range(n + 1)]
 for _ in range(m):
     u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    adjacent_list[u].append(v)
+    adjacent_list[v].append(u)
 
 visited = [False] * (n + 1)
 components = 0
